353b2c5d856633c50f971cc8a4628cb244faec5b
Łukasz Męderak <lukasz.mederak@gmail.com>
2025-12-20 16:18:49 +0100

    Change user centrics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,17 +8,17 @@ import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import Script from "next/script";
 
 const figtree = Figtree({ subsets: ["latin", "latin-ext"] });
-const raleway = Raleway({
-  subsets: ["latin", "latin-ext"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], // Specify the desired weights
-});
-const dmSans = DM_Sans({
-  subsets: ["latin", "latin-ext"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], // Specify the desired weights
-});
+// const raleway = Raleway({
+//   subsets: ["latin", "latin-ext"],
+//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], // Specify the desired weights
+// });
+// const dmSans = DM_Sans({
+//   subsets: ["latin", "latin-ext"],
+//   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], // Specify the desired weights
+// });
 
 export const metadata: Metadata = {
-  title: "Łukasz Męderak | Trener",
+  title: "Łukasz Męderak | Trener personalny",
   description:
     "Treningi personalne Warszawa Wola ul.Jana Kazimierza. Jeśli chcesz rozpocząć treningi na siłowni pod okiem trenera personalnego, poprawić zdrowie, uzyskać lepszą sylwetkę czy polepszyć wyniki w danej dyscyplinie sportu, to chętnie Ci w tym pomogę.",
   openGraph: {
@@ -38,10 +38,10 @@ export const metadata: Metadata = {
     locale: "pl",
     type: "website",
   },
-  other: {
-    "trustpilot-one-time-domain-verification-id":
-      "57c620d0-4ee3-4fc0-8b7a-04ea80631980",
-  },
+  // other: {
+  //   "trustpilot-one-time-domain-verification-id":
+  //     "57c620d0-4ee3-4fc0-8b7a-04ea80631980",
+  // },
 };
 
 export default function RootLayout({
@@ -55,15 +55,15 @@ export default function RootLayout({
       {/* rose, lime, yellow, blue, indigo */}
       <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GTM_ID as string} />
       <head>
-        {isProduction && (
+        {/* {isProduction && (
           <>
-            {/* Usercentrics - autoblocker */}
+            Usercentrics - autoblocker
             <Script
               src="https://web.cmp.usercentrics.eu/modules/autoblocker.js"
               strategy="beforeInteractive"
             />
 
-            {/* Usercentrics - banner */}
+            Usercentrics - banner
             <Script
               id="usercentrics-cmp"
               src="https://web.cmp.usercentrics.eu/ui/loader.js"
@@ -72,7 +72,7 @@ export default function RootLayout({
               async
             />
           </>
-        )}
+        )} */}
       </head>
       <body className={twMerge(` bg-defaultWhite`, figtree.className)}>
         <AlertProvider>
